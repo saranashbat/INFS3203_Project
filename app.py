c0bfc4746203a627687915d73d2f3f204d39d99d
saranashbat <146200609+saranashbat@users.noreply.github.com>
2024-03-25 20:27:12 +0300

    flask app

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/infotechjobs')
+def infoTechJobs():
+    return render_template('infotechjobs.html')
+
+@app.route('/infotechjob1')
+def infoTechJob1():
+    return render_template('infotechjob1.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
