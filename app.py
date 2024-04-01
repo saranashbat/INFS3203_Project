c73d07b9dcfe0a37391dda0c92519ed70058105c
Heba Othman <60092554@udst.edu.qa>
2024-04-02 02:23:34 +0300

    profile,signup

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
-from flask import Flask, render_template, request, redirect, url_for
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask import Flask, render_template, request, redirect
+from flask_login import LoginManager, current_user, login_user, login_required
 
 app = Flask(__name__)
-app.secret_key = '1234'
-
-login_manager = LoginManager()
-login_manager.init_app(app)
+app.secret_key = 'your_secret_key'  # Add a secret key for session security
+login_manager = LoginManager(app)
 
 # Mock user database
-class User(UserMixin):
-    def __init__(self, id):
+class User:
+    def __init__(self, id, username, email, fullname):
         self.id = id
+        self.username = username
+        self.email = email
+        self.fullname = fullname
 
-users = {'user': User('user')}  
+users = {'example_user': User('1', 'example_user', 'example@example.com', 'Example User')}
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -25,49 +26,45 @@ def home():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        if username in users and password == 'password':
-            user = users[username]
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = users.get(username)
+        if user and password == 'example_password':  # Add your authentication logic here
             login_user(user)
-            return redirect(url_for('home'))
-    return render_template('login.html')
+            return redirect('/')
+        else:
+            return render_template('login.html', error='Invalid username or password')
+    else:
+        return render_template('login.html')
 
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('home'))
+@app.route('/SignUp', methods=['GET', 'POST'])
+def SignUp():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        email = request.form.get('email')
+        fullname = request.form.get('fullname')
+        # Process sign up data here
+        # Example: Save the new user to the database
+        return redirect('/')  # Redirect to the home page after successful sign up
+    else:
+        return render_template('SignUp.html')
 
 @app.route('/profile')
 @login_required
 def profile():
     user_info = {
-        'username': current_user.id, 
-        'email': 'user@user.com',  
-        'fullname': 'user'         
+        'username': current_user.username,
+        'email': current_user.email,
+        'fullname': current_user.fullname
     }
     return render_template('profile.html', user_info=user_info)
 
-@app.route('/SignUp', methods=['GET', 'POST'])  
-def SignUp():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        email = request.form['email']
-        fullname = request.form['fullname']
-        # Process sign up data here
-        return redirect(url_for('home'))
-    else:
-        return render_template('SignUp.html')
-
 @app.route('/infotechjobs')
-@login_required
 def infoTechJobs():
     return render_template('infotechjobs.html')
 
 @app.route('/infotechjob1')
-@login_required
 def infoTechJob1():
     return render_template('infotechjob1.html')
 
